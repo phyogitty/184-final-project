cb4e4006b1f87cfa8d707c79ed6937a2ccef1e7d
fbwarren <fbwarren@berkeley.edu>
2021-04-19 18:58:50 -0500

    add project 4

---
+++ clothsim/src/collision/plane.cpp
@@ -0,0 +1,59 @@
+#include "iostream"
+#include <nanogui/nanogui.h>
+
+#include "../clothMesh.h"
+#include "../clothSimulator.h"
+#include "plane.h"
+
+using namespace std;
+using namespace CGL;
+
+#define SURFACE_OFFSET 0.0001
+
+void Plane::collide(PointMass &pm) {
+    double before = dot((pm.last_position - point), normal);
+    double after = dot((pm.position - point), normal);
+    // Comparing dot product signs to see if plane was intersected between last_position and position
+    if (before < 0 ^ after < 0) {
+        // if dot product for position was positive, then position is in the same direction as the plane's
+        // normal, i.e. for positive dot product we move position opposite of the normal, for negative dot product
+        // we move in the same direction as the plane's normal
+        Vector3D tangent = pm.position - after * normal.unit();
+        Vector3D correction = (tangent - pm.last_position) + (SURFACE_OFFSET * normal);
+        pm.position = pm.last_position + correction * (1 - friction);
+    }
+}
+
+void Plane::render(GLShader &shader) {
+  nanogui::Color color(0.7f, 0.7f, 0.7f, 1.0f);
+
+  Vector3f sPoint(point.x, point.y, point.z);
+  Vector3f sNormal(normal.x, normal.y, normal.z);
+  Vector3f sParallel(normal.y - normal.z, normal.z - normal.x,
+                     normal.x - normal.y);
+  sParallel.normalize();
+  Vector3f sCross = sNormal.cross(sParallel);
+
+  MatrixXf positions(3, 4);
+  MatrixXf normals(3, 4);
+
+  positions.col(0) << sPoint + 2 * (sCross + sParallel);
+  positions.col(1) << sPoint + 2 * (sCross - sParallel);
+  positions.col(2) << sPoint + 2 * (-sCross + sParallel);
+  positions.col(3) << sPoint + 2 * (-sCross - sParallel);
+
+  normals.col(0) << sNormal;
+  normals.col(1) << sNormal;
+  normals.col(2) << sNormal;
+  normals.col(3) << sNormal;
+
+  if (shader.uniform("u_color", false) != -1) {
+    shader.setUniform("u_color", color);
+  }
+  shader.uploadAttrib("in_position", positions);
+  if (shader.attrib("in_normal", false) != -1) {
+    shader.uploadAttrib("in_normal", normals);
+  }
+
+  shader.drawArray(GL_TRIANGLE_STRIP, 0, 4);
+}
